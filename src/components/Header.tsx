0f8d1b9d3023a79fc7b688df3519deea82cd3e16
Padam7890 <padamthapamoho@gmail.com>
2024-09-13 16:33:48 +0545

    logo changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import chillLogo from "../../public/logo/ChillCan.png"
 
 type Props = {};
 
@@ -7,7 +8,7 @@ export default function Header({}: Props) {
   return (
     <header className="-mb-28 flex justify-center py-4">
       <div className="z-10 h-20 cursor-pointer text-sky-800" >
-        <Image alt="chill Can Png" src={"/logo/chill Can.png"}  height={300} width={140}/>
+        <Image alt="chill Can Png" src={chillLogo}  height={300} width={140}/>
         </div>
     </header>
   );
